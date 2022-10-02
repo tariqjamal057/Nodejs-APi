50cb2a68c9bb21ed8bcb080865cb55d7a0120cb4
Tariq Jamal A <tariqjamal4267324@gmail.com>
2022-10-02 18:11:31 +0530

    upd

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const userRoute = require("./routes/user.route");
 const actRoute = require("./routes/act.route");
 const complaintRoute = require("./routes/complaint.route");
 const hrdRoute = require("./routes/hrd.routes");
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: "*"
+}))
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
@@ -19,4 +24,6 @@ app.get("/", (req, res) => {
   res.send('App is working')
 });
 
-app.listen(3001);
+app.listen(3001,()=> {
+  console.log("Server running at 3001");
+});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,141 +1,198 @@
 const express = require("express");
 const routes = express.Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("../config/database");
 const middleware = require("../middleware/user.middleware");
 const token = require("../middleware/token.middleware");
 var nodemailer = require("nodemailer");
 
-routes.post("/register", middleware.validateRegister, (req, res, next) => {
-  const name = req.body.name;
-  const dob = req.body.dob;
-  const gender = req.body.gender;
-  const qualification = req.body.qualification;
-  const address = req.body.address;
-  const email = req.body.email;
-  const password = req.body.password;
-  const phone_number = req.body.phonenumber;
-  const company_name = req.body.companyname;
-  const location = req.body.location;
-  let c_id = 0;
+// routes.post("/register", middleware.validateRegister, (req, res, next) => {
+//   const name = req.body.name;
+//   const dob = req.body.dob;
+//   const gender = req.body.gender;
+//   const qualification = req.body.qualification;
+//   const address = req.body.address;
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   const phone_number = req.body.phonenumber;
+//   const company_name = req.body.companyname;
+//   const location = req.body.location;
+//   let c_id = 0;
 
-  db.query(
-    `SELECT * FROM ttuser WHERE email = ${db.escape(email)}`,
-    (err, result) => {
-      if (result.length) {
-        return res.status(409).send({
-          message: "Email is already in use",
-        });
-      } else {
-        bcrypt.hash(password, 10, (err, hash) => {
-          if (err) {
-            // throw err;
-            return res.status(500).send({
-              message: err,
-            });
-          } else {
-            db.query(
-              `SELECT * FROM tmcompany WHERE comname = ${db.escape(
-                company_name
-              )}`,
-              (c_err, c_res) => {
-                if (c_err) {
-                  return res.status(400).send({
-                    message: "Something went wrong",
-                  });
-                }
-                if (c_res.length) {
-                  console.log(c_res[0].comid);
-                  c_id = c_res[0].comid;
-                  db.query(
-                    `SELECT * FROM tmdistrict WHERE district = ${db.escape(
-                      location
-                    )}`,
-                    (l_err, l_res) => {
-                      if (l_err) {
-                        return res.status(400).send({
-                          message: "Something went wrong",
-                        });
-                      }
-                      if (l_res.length) {
-                        console.log(l_res[0].did);
-                        l_id = l_res[0].did;
-                        db.query(
-                          `SELECT * FROM tmqualification WHERE qualification = ${db.escape(
-                            qualification
-                          )}`,
-                          (q_err, q_res) => {
-                            if (q_err) {
-                              return res.status(400).send({
-                                message: "Something went wrong",
-                              });
-                            }
-                            if (q_res.length) {
-                              console.log(q_res[0].qid);
-                              q_id = q_res[0].qid;
+//   db.query(
+//     `SELECT * FROM ttuser WHERE email = ${db.escape(email)}`,
+//     (err, result) => {
+//       if (result.length) {
+//         return res.status(409).send({
+//           message: "Email is already in use",
+//         });
+//       } else {
+//         bcrypt.hash(password, 10, (err, hash) => {
+//           if (err) {
+//             // throw err;
+//             return res.status(500).send({
+//               message: err,
+//             });
+//           } else {
+//             db.query(
+//               `SELECT * FROM tmcompany WHERE comname = ${db.escape(
+//                 company_name
+//               )}`,
+//               (c_err, c_res) => {
+//                 if (c_err) {
+//                   return res.status(400).send({
+//                     message: "Something went wrong",
+//                   });
+//                 }
+//                 if (c_res.length) {
+//                   console.log(c_res[0].comid);
+//                   c_id = c_res[0].comid;
+//                   db.query(
+//                     `SELECT * FROM tmdistrict WHERE district = ${db.escape(
+//                       location
+//                     )}`,
+//                     (l_err, l_res) => {
+//                       if (l_err) {
+//                         return res.status(400).send({
+//                           message: "Something went wrong",
+//                         });
+//                       }
+//                       if (l_res.length) {
+//                         console.log(l_res[0].did);
+//                         l_id = l_res[0].did;
+//                         db.query(
+//                           `SELECT * FROM tmqualification WHERE qualification = ${db.escape(
+//                             qualification
+//                           )}`,
+//                           (q_err, q_res) => {
+//                             if (q_err) {
+//                               return res.status(400).send({
+//                                 message: "Something went wrong",
+//                               });
+//                             }
+//                             if (q_res.length) {
+//                               console.log(q_res[0].qid);
+//                               q_id = q_res[0].qid;
 
-                              db.query(
-                                `INSERT INTO ttuser(name,email,password,phonenumber,dob,gender,qualification,address,companyname,location) VALUES (
-                                      ${db.escape(name)},
-                                        ${db.escape(email)},
-                                        ${db.escape(hash)},
-                                        ${db.escape(phone_number)},
-                                        ${db.escape(dob)},
-                                        ${db.escape(gender)},
-                                        ${db.escape(q_id)},
-                                        ${db.escape(address)},
-                                        ${db.escape(c_id)},
-                                        ${db.escape(l_id)}
-                                        );`,
-                                (err, result) => {
-                                  if (err) {
-                                    throw err;
-                                    return res.status(400).send({
-                                      message: err,
-                                    });
-                                  }
-                                  console.log(result);
-                                  return res.status(201).send({
-                                    message: "Registered",
-                                  });
-                                }
-                              );
-                            }
-                          }
-                        );
-                      }
-                    }
-                  );
-                }
-              }
-            );
-          }
-        });
-      }
-    }
-  );
-});
+//                               db.query(
+//                                 `INSERT INTO ttuser(name,email,password,phonenumber,dob,gender,qualification,address,companyname,location) VALUES (
+//                                       ${db.escape(name)},
+//                                         ${db.escape(email)},
+//                                         ${db.escape(hash)},
+//                                         ${db.escape(phone_number)},
+//                                         ${db.escape(dob)},
+//                                         ${db.escape(gender)},
+//                                         ${db.escape(q_id)},
+//                                         ${db.escape(address)},
+//                                         ${db.escape(c_id)},
+//                                         ${db.escape(l_id)}
+//                                         );`,
+//                                 (err, result) => {
+//                                   if (err) {
+//                                     throw err;
+//                                     return res.status(400).send({
+//                                       message: err,
+//                                     });
+//                                   }
+//                                   console.log(result);
+//                                   return res.status(201).send({
+//                                     message: "Registered",
+//                                   });
+//                                 }
+//                               );
+//                             }
+//                           }
+//                         );
+//                       }
+//                     }
+//                   );
+//                 }
+//               }
+//             );
+//           }
+//         });
+//       }
+//     }
+//   );
+// });
 
-const updateLastLogin = (email) => {
-  db.query(
-    `UPDATE ttuser SET 
-  _islastlogin = NOW() WHERE email = ${db.escape(email)}
-  `,
-    (u_err, u_response) => {
-      if (u_err) {
-        throw u_err;
-      }
-      console.log(u_response);
-    }
-  );
-};
+// const updateLastLogin = (email) => {
+//   db.query(
+//     `UPDATE ttuser SET 
+//   _islastlogin = NOW() WHERE email = ${db.escape(email)}
+//   `,
+//     (u_err, u_response) => {
+//       if (u_err) {
+//         throw u_err;
+//       }
+//       console.log(u_response);
+//     }
+//   );
+// };
+
+// routes.post("/login", middleware.validateLogin, (req, res, next) => {
+//   console.log("75")
+//   console.log(req.body.email)
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   db.query(
+//     `SELECT * FROM ttuser WHERE email = ${db.escape(email)}`,
+//     (err, result) => {
+//       console.log(result);
+//       if (err) {
+//         throw err;
+//         return res.send({
+//           message: err,
+//         });
+//       }
+//       if (!result.length) {
+//         res.render("profile.ejs", { message: "Email is incorrect" });
+//       } else {
+//         bcrypt.compare(password, result[0]["password"], (error, hashresult) => {
+//           if (error) {
+//             throw error;
+//             return res.send({
+//               message: "Username is incorrect",
+//             });
+//           }
+//           if (hashresult) {
+//             const token = jwt.sign(
+//               {
+//                 username: result[0].username,
+//                 id: result[0].id,
+//               },
+//               "hello",
+//               {
+//                 expiresIn: "1d",
+//               }
+//             );
+//             // updateLastLogin(email);
+//             if (result[0].role === "employee") {
+//               return res.status(200).send({
+//                 data: result[0],
+//               });
+//             } else {
+//               return res.status(200).send({
+//                 message: "Loggedin",
+//                 token,
+//                 data: result[0],
+//               });
+//             }
+//           }
+//           console.log(hashresult);
+//           return res.status(200).send({
+//             message: "Password is incorrect",
+//           });
+//         });
+//       }
+//     }
+//   );
+// });
 
 routes.post("/login", middleware.validateLogin, (req, res, next) => {
-  console.log("75")
-  console.log(req.body.email)
   const email = req.body.email;
-  const password = req.body.password;
+  const password = '123';
   db.query(
     `SELECT * FROM ttuser WHERE email = ${db.escape(email)}`,
     (err, result) => {
@@ -146,49 +203,34 @@ routes.post("/login", middleware.validateLogin, (req, res, next) => {
           message: err,
         });
       }
-      if (!result.length) {
-        res.render("profile.ejs", { message: "Email is incorrect" });
-      } else {
-        bcrypt.compare(password, result[0]["password"], (error, hashresult) => {
-          if (error) {
-            throw error;
-            return res.send({
-              message: "Username is incorrect",
-            });
+      if (result.length) {
+        const token = jwt.sign(
+          {
+            username: result.username,
+            id: result.id,
+          },
+          "hello",
+          {
+            expiresIn: "1d",
           }
-          if (hashresult) {
-            const token = jwt.sign(
-              {
-                username: result[0].username,
-                id: result[0].id,
-              },
-              "hello",
-              {
-                expiresIn: "1d",
-              }
-            );
-            // updateLastLogin(email);
-            if (result[0].role === "employee") {
-              return res.status(200).send({
-                data: result[0],
-              });
-            } else {
-              return res.status(200).send({
-                message: "Loggedin",
-                token,
-                data: result[0],
-              });
-            }
-          }
-          console.log(hashresult);
+        );
+        // updateLastLogin(email);
+        if (result.role === "employee") {
           return res.status(200).send({
-            message: "Password is incorrect",
+            data: result,
           });
-        });
-      }
+        } else {
+          return res.status(200).send({
+            message: "Loggedin",
+            token,
+            data: result,
+          });
+        }
+      } 
     }
   );
 });
+
 
 routes.patch("/profile/update", token.checkToken, (req, res, next) => {
   const qualification = req.body.qualification;
