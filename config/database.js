9a87a46758c9fe64eb56dab11fc835bd3fe73e2e
Tariq Jamal A <tariqjamal4267324@gmail.com>
2022-10-02 18:28:41 +0530

    upd

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    database: 'codealpha',
-    password: 'Jamal@123',
-    port: '3300'
-});
+// const db = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     database: 'codealpha',
+//     password: 'Jamal@123',
+//     port: '3300'
+// });
 db.connect();
 
 module.exports = db;
