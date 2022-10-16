48bc73f16bacd5b8f9ca56a4266255683f2916f1
Tariq Jamal A <tariqjamal4267324@gmail.com>
2022-10-16 20:30:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 // const db = require("./config/database");
 
-const adminRoute = require("./routes/admin.route");
-const userRoute = require("./routes/person.route");
+// const adminRoute = require("./routes/admin.route");
+// const userRoute = require("./routes/person.route");
 
 const app = express();
 app.use(express.json());
@@ -13,8 +13,8 @@ app.use(
   })
 );
 
-app.use("/admin", adminRoute);
-app.use("/user", userRoute);
+// app.use("/admin", adminRoute);
+// app.use("/user", userRoute);
 
 app.get("/", (req, res) => {
   res.json("App is working");
